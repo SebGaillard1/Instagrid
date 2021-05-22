bc1a8f8522a5ab5bba37a33da023482bfb81d8c0
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2021-05-22 09:48:21 +0200

    UIImagePickerAdded

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -8,16 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    
-    
-
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func buttonsImageGrid(_ sender: UIButton) {
+        
+        let myPickerController = UIImagePickerController()
+        myPickerController.sourceType = .photoLibrary
+        myPickerController.delegate = self
+        myPickerController.allowsEditing = true
+        //myPickerController.mediaTypes = []
 
+        self.present(myPickerController, animated: true, completion: nil)
+        //sender.setBackgroundImage(#imageLiteral(resourceName: "Icon"), for: .normal)
+        sender.setImage(nil, for: .normal)
+    }
+    
+    func getImage(image: UIImage) {
+        
+    }
+}
 
+extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            getImage(image: image)
+            
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
